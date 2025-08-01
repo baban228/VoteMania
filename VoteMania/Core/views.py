47f44bc989e4df3863e773e0935778f5e6cb5edc
Karsanov Artem <karsanov05@inbox.ru>
2025-08-01 16:18:57 +0300

    start job

---
+++ VoteMania/Core/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponseNotFound
+# Create your views here.
+
+
+def index(request):
+    return render(request, "Core/index.html")
+
+
+def page_not_found(request, exception):
+    return render(request, "Not_Found/index.html")
